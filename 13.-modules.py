e6b4d78e42cc4830d661e7feda60e885cd998238
clarito2021 <citizenlex2016@gmail.com>
2021-08-17 18:09:29 -0400

    .

--- 13.-modules.py
+++ 13.-modules.py
@@ -3,6 +3,7 @@
 # third party modules
 
 # python modules
+from colorama import Fore, Style, init
 import fmath
 from datetime import timedelta, date
 import datetime
@@ -17,3 +18,14 @@ fmath.substract(10, 8)
 fmath.substract(9, 9)
 fmath.substract(1, 10)
 fmath.add(123, -122)
+
+
+print("---------------------------------")
+
+
+init(convert=True)
+print(Fore.RED+"Hello World")
+print(Fore.BLUE+"Hello World")
+print(Fore.GREEN+"Hello World")
+print(Fore.CYAN+"Hello World")
+print(Fore.YELLOW+"Hello World")
