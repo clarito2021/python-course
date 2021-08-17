52bb1b7ffd84c22f9b80bfa67b9a90a808a802cd
clarito2021 <citizenlex2016@gmail.com>
2021-08-17 17:25:28 -0400

    módulos y funciones

---
+++ 12.-functions.py
@@ -0,0 +1,40 @@
+x = 30.41
+print("asdasdasdad")
+dir(x)
+type(12)
+
+print("*********************************")
+
+
+def hello(name="Dave"):
+    print("Hello" + " " + name)
+
+
+hello("Carlos")
+hello("Lorena")
+
+
+print("*********************************")
+
+
+hello()
+
+
+print("*********************************")
+
+
+def add(numberOne, numberTwo):
+    return numberOne + numberTwo
+
+
+print(add(10, 30))
+print(add(600, 345345))
+
+
+print("*********************************")
+
+
+def add02(numberOne, numberTwo): return numberOne + numberTwo
+
+
+print(add02(20, 49))
---
+++ 13.-modules.py
@@ -0,0 +1,19 @@
+# own modules
+
+# third party modules
+
+# python modules
+import fmath
+from datetime import timedelta, date
+import datetime
+
+print(datetime.date.today())
+print(datetime.timedelta(minutes=100))
+
+print(timedelta(minutes=120))
+
+
+fmath.substract(10, 8)
+fmath.substract(9, 9)
+fmath.substract(1, 10)
+fmath.add(123, -122)
